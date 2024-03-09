1b3306f4f0c408e2a7ef888c08dc22a1183360bb
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-03-09 17:39:52 +0200

    Adding Vehicle entity with the relevant custom validation attribute for the license plate.

---
+++ SmartParkInnovate.Infrastructure/Data/Attributes/LicensePlateFormatAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SmartParkInnovate.Infrastructure.Data.Attributes
+{
+    public class LicensePlateFormatAttribute : ValidationAttribute
+    {
+
+        public override bool IsValid(object? value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+
+            string licensePlate = value.ToString();
+
+            string[] patternTypes = new string[]
+            {
+                @"^([A-Z]{2}[0-9]{4}[A-Z]{2})$",
+                @"^([A-Z]{1}[0-9]{4}[A-Z]{2})$",
+                @"^([A-Z]{2}[0-9]{6})$",
+                @"^([A-Z]{1}[0-9]{5}[A-Z]{2})$",
+                @"^([A-Z]{1}[0-9]{7})$"
+            };
+
+
+            for (int i = 0; i < patternTypes.Length; i++)
+            {
+                if(Regex.Match(licensePlate, patternTypes[i]).Success)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
--- SmartParkInnovate.Infrastructure/Data/Models/Department.cs
+++ SmartParkInnovate.Infrastructure/Data/Models/Department.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace SmartParkInnovate.Infrastructure.Data.Models
 {
-    internal class Department
+    public class Department
     {
+        public Department()
+        {
+            this.Workers = new List<Worker>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Department name must be between 3 and 100 characters long.")]
+        public string Name { get; set; } = null!;
+
+
+        [InverseProperty(nameof(Department))]
+        public ICollection<Worker> Workers { get; set; }
     }
 }
