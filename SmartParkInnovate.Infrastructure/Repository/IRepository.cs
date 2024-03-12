49e4924679be3bdab250ae008f9669f291743a88
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-03-12 22:46:39 +0200

    Implementing repository in order to get the data from the repo instead of the context directly.

---
+++ SmartParkInnovate.Infrastructure/Repository/IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartParkInnovate.Infrastructure.Repository
+{
+    public interface IRepository
+    {
+        public IQueryable<T> All<T>();
+
+        public IQueryable<T> AllAsReadOnly<T>();
+    }
+}
