1c3b8478cf8af41b7bc78eeab81be1f2550ba272
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-03-16 00:51:01 +0200

    Adding Contracts, Services and Models folder to business layer. Creating IParkingService

--- SmartParkInnovate.Infrastructure/Migrations/20240315224258_AddingIDeletableToPostAndVehicle.Designer.cs
+++ SmartParkInnovate.Infrastructure/Migrations/20240315224258_AddingIDeletableToPostAndVehicle.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SmartParkInnovate.Data;
 
@@ -11,9 +12,10 @@ using SmartParkInnovate.Data;
 namespace SmartParkInnovate.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240315224258_AddingIDeletableToPostAndVehicle")]
+    partial class AddingIDeletableToPostAndVehicle
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SmartParkInnovate.Infrastructure/Migrations/20240315224258_AddingIDeletableToPostAndVehicle.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartParkInnovate.Infrastructure.Migrations
+{
+    public partial class AddingIDeletableToPostAndVehicle : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EnterDate",
+                table: "ParkingSpotOccupations");
+
+            migrationBuilder.RenameColumn(
+                name: "ExitDate",
+                table: "ParkingSpotOccupations",
+                newName: "EnterDateTime");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedOn",
+                table: "Vehicles",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Vehicles",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedOn",
+                table: "Posts",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Posts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExitDateTime",
+                table: "ParkingSpotOccupations",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedOn",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Vehicles");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedOn",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "ExitDateTime",
+                table: "ParkingSpotOccupations");
+
+            migrationBuilder.RenameColumn(
+                name: "EnterDateTime",
+                table: "ParkingSpotOccupations",
+                newName: "ExitDate");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EnterDate",
+                table: "ParkingSpotOccupations",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
