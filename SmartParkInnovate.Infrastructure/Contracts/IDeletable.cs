9a52ebc7cbe8d1eba926e87ac83d6bf326648446
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-03-16 00:35:38 +0200

    Adding IDeletable interface so that Post and vehicle can implement it

---
+++ SmartParkInnovate.Infrastructure/Contracts/IDeletable.cs
@@ -0,0 +1,8 @@
+﻿namespace SmartParkInnovate.Infrastructure.Contracts
+{
+    public interface IDeletable
+    {
+        public bool IsDeleted { get; set; }
+        public DateTime DeletedOn { get; set; }
+    }
+}
