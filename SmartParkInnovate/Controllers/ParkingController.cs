0d5603762b6b438af574a6abf721200258593f11
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-03-16 18:56:40 +0200

    Adding All method to parking controller

--- SmartParkInnovate/Controllers/ParkingController.cs
+++ SmartParkInnovate/Controllers/ParkingController.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SmartParkInnovate.Core.Contracts;
 
 namespace SmartParkInnovate.Controllers
 {
     public class ParkingController : Controller
     {
-        public IActionResult All()
+        private readonly IParkingService parkingService;
+
+        public ParkingController(IParkingService parkingService)
         {
+            this.parkingService = parkingService;
+        }
+
+
+        public async Task<IActionResult> All()
+        {
+            var parkingSpots = await this.parkingService.All();
             return View(nameof(All));
         }
     }
