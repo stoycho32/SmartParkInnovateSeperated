df4adb96c0e441042bade8017ba6bc12af5aa948
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-03-16 18:05:17 +0200

    Adding ParkingSpotViewModel in the business layer

---
+++ SmartParkInnovate.Core/Models/ParkingSpot/ParkingSpotViewModel.cs
@@ -0,0 +1,22 @@
+﻿using SmartParkInnovate.Infrastructure.Data.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SmartParkInnovate.Core.Models.ParkingSpot
+{
+    public class ParkingSpotViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public bool IsEnabled { get; set; }
+
+        [Required]
+        public bool IsOccupied { get; set; }
+
+
+        public int? OccupationVehicleId { get; set; }
+
+        public Vehicle? OccupationVehicle { get; set; }
+    }
+}
