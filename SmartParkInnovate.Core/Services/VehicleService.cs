b99886c9e2eb05a193e7b31ca5c09b2465a41bf0
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-03-17 20:33:55 +0200

    Adding Vehicle Service

---
+++ SmartParkInnovate.Core/Services/VehicleService.cs
@@ -0,0 +1,19 @@
+﻿using SmartParkInnovate.Infrastructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartParkInnovate.Core.Services
+{
+    public class VehicleService
+    {
+        private readonly IRepository repository;
+
+        public VehicleService(IRepository repository)
+        {
+            this.repository = repository;
+        }
+    }
+}
