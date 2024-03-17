a6f57867dd6e0771c9b5079011e756214918d3e5
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-03-17 19:51:41 +0200

    Creating VehicleFormModel

---
+++ SmartParkInnovate.Core/Models/VehicleModel/VehicleFormModel.cs
@@ -0,0 +1,10 @@
+﻿using SmartParkInnovate.Infrastructure.Data.Attributes;
+
+namespace SmartParkInnovate.Core.Models.Vehicle
+{
+    public class VehicleFormModel
+    {
+        [LicensePlateFormat]
+        public string LicensePlate { get; set; }
+    }
+}
