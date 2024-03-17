617cd2798c190d61c1b107daeb2d25b83ad4ced7
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-03-17 20:49:38 +0200

    Implementing IVehicle Service

--- SmartParkInnovate.Core/Services/VehicleService.cs
+++ SmartParkInnovate.Core/Services/VehicleService.cs
@@ -1,8 +1,9 @@
-﻿using SmartParkInnovate.Infrastructure.Repository;
+﻿using SmartParkInnovate.Core.Contracts;
+using SmartParkInnovate.Infrastructure.Repository;
 
 namespace SmartParkInnovate.Core.Services
 {
-    public class VehicleService
+    public class VehicleService : IVehicleService
     {
         private readonly IRepository repository;
 
@@ -11,5 +12,29 @@ namespace SmartParkInnovate.Core.Services
             this.repository = repository;
         }
 
+        public Task Add(string make, string model, string licensePlate, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task All()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Details()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task MyVehicles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Remove()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
