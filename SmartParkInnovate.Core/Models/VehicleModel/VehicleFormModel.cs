16ad9c5978cdd3ac692a2c0f203240167acd11a6
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-03-21 19:22:45 +0200

    Creating Vehicle Form Model

--- SmartParkInnovate.Core/Models/VehicleModel/VehicleFormModel.cs
+++ SmartParkInnovate.Core/Models/VehicleModel/VehicleFormModel.cs
@@ -1,10 +1,29 @@
 ﻿using SmartParkInnovate.Infrastructure.Data.Attributes;
+using SmartParkInnovate.Infrastructure.Data.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static SmartParkInnovate.Infrastructure.Data.Constants.DataConstants;
+using static SmartParkInnovate.Infrastructure.Data.Constants.ErrorMessages;
 
 namespace SmartParkInnovate.Core.Models.VehicleModel
 {
     public class VehicleFormModel
     {
+        [Required]
+        [StringLength(VehicleDataConstants.VehicleMakeMaxLength,
+            MinimumLength = VehicleDataConstants.VehicleMakeMinLength)]
+        public string Make { get; set; } = null!;
+
+
+        [Required]
+        [StringLength(VehicleDataConstants.VehicleModelMaxLength,
+            MinimumLength = VehicleDataConstants.VehicleModelMinLength,
+            ErrorMessage = VehicleErrorMessages.VehicleModelLengthErrorMessage)]
+        public string Model { get; set; } = null!;
+
+
+        [Required]
         [LicensePlateFormat]
-        public string LicensePlate { get; set; }
+        public string LicensePlate { get; set; } = null!;
     }
 }
