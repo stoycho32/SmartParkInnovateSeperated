09fadc7fea80f465485ddb1a1d712ab11daa45a2
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-03-21 19:17:53 +0200

    Creating VehicleController

---
+++ SmartParkInnovate/Controllers/VehicleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartParkInnovate.Controllers
+{
+    public class VehicleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
