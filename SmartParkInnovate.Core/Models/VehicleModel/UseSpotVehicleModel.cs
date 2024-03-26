4763efe698ab2c619215c53127b2048fbc989c0c
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-03-26 10:16:18 +0200

    Reversing to an older commit

---
+++ SmartParkInnovate.Core/Models/VehicleModel/UseSpotVehicleModel.cs
@@ -0,0 +1,12 @@
+﻿using SmartParkInnovate.Infrastructure.Data.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace SmartParkInnovate.Core.Models.VehicleModel
+{
+    public class UseSpotVehicleModel
+    {
+        [Required]
+        [LicensePlateFormat]
+        public string LicensePlate { get; set; } = null!;
+    }
+}
