28793672d4b60ebb93b627356ec14671060e85bc
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-03-26 10:19:31 +0200

    Removing Worker view model

--- SmartParkInnovate.Core/Models/WorkerModel/WorkerViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SmartParkInnovate.Core.Models.WorkerModel
-{
-    public class WorkerViewModel
-    {
-        public string Id { get; set; }
-
-        public string UserName { get; set; }
-    }
-}
