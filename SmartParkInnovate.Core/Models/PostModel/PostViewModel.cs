a699b33bf18ca609d1ff2405f7fd0f91f174e27b
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-03-28 21:03:30 +0200

    Creating Posts html view and creating the post view model. Adding the post view model path to the importusings file.

---
+++ SmartParkInnovate.Core/Models/PostModel/PostViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static SmartParkInnovate.Infrastructure.Data.Constants.DataConstants;
+using static SmartParkInnovate.Infrastructure.Data.Constants.ErrorMessages;
+
+namespace SmartParkInnovate.Core.Models.PostModel
+{
+    public class PostViewModel
+    {
+        [Required]
+        public string WorkerUserName { get; set; } = null!;
+
+        [Required]
+        [StringLength(PostDataConstants.PostBodyMaxValue,
+            MinimumLength = PostDataConstants.PostBodyMaxValue,
+            ErrorMessage = PostErrorMessages.PostBodyErrorMessage)]
+        public string PostBody { get; set; } = null!;
+
+
+        [Required]
+        public DateTime PostDate { get; set; }
+
+        [Required]
+        public int LikesCount { get; set; }
+
+        [Required]
+        public int CommentsCount { get; set; }
+    }
+}
