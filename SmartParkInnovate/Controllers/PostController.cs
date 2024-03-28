a05708c7e6206dd111b8cea692a578df6d2e28dc
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-03-28 21:03:52 +0200

    Creating Post Controller to add the all posts http get method.

---
+++ SmartParkInnovate/Controllers/PostController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartParkInnovate.Core.Contracts;
+using SmartParkInnovate.Core.Models.PostModel;
+
+namespace SmartParkInnovate.Controllers
+{
+    public class PostController : BaseController
+    {
+        private readonly IPostService postService;
+
+        public PostController(IPostService postService)
+        {
+            this.postService = postService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Posts()
+        {
+            List<PostViewModel> posts = await this.postService.All();
+
+            return View(posts);
+        }
+    }
+}
