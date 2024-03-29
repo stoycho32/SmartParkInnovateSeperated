f4f3d62b7b972b1d3f6425b30806bbafc7e45e00
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-03-29 19:03:38 +0200

    Adding a custom format attribute for post body

---
+++ SmartParkInnovate.Infrastructure/Data/Attributes/PostFormatAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace SmartParkInnovate.Infrastructure.Data.Attributes
+{
+    public class PostFormatAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            string objectValueToString = value.ToString();
+            string patternType = @"\w";
+
+
+            if (!Regex.Match(objectValueToString, patternType).Success)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
