44f058b37344bf27336947e541daeee9f9f9b7b4
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-04-03 23:05:54 +0300

    Adding antiforgerytokenattribute to the services

--- SmartParkInnovate/Program.cs
+++ SmartParkInnovate/Program.cs
@@ -1,19 +1,29 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplicationDbContext(builder.Configuration);
 builder.Services.AddApplicationIdentity(builder.Configuration);
-builder.Services.AddControllersWithViews();
+
+builder.Services.AddControllersWithViews(options =>
+{ 
+    options.Filters.Add<AutoValidateAntiforgeryTokenAttribute>();
+});
+
 builder.Services.AddApplicationServices();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseMigrationsEndPoint();
 }
 else
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Home/Error/500");
+    app.UseStatusCodePagesWithReExecute("/Home/Error?statuscode ={0}");
     app.UseHsts();
 }
 
