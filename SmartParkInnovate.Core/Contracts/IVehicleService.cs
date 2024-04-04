10dcc86c7ad3f8fd8a9108b8cf5e768cd9351818
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-04-04 20:51:50 +0300

    Returning the all vehicles functionality to the contract

--- SmartParkInnovate.Core/Contracts/IVehicleService.cs
+++ SmartParkInnovate.Core/Contracts/IVehicleService.cs
@@ -8,6 +8,8 @@ namespace SmartParkInnovate.Core.Contracts
 
         public Task<VehicleDetailsViewModel> Details(int id);
 
+        public Task<List<VehicleViewModel>> All();
+
         public List<VehicleViewModel> MyVehicles(string userId);
 
         public Task Remove(int id);
