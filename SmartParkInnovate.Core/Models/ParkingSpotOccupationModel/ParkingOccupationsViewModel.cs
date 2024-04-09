573cdf544ac4320b9417387e33d81374d42f2c97
stoycho karadaliev <karadalievstoycho@gmail.com>
2024-04-09 21:53:22 +0300

    Adding Parking Occupations view model for the admin

---
+++ SmartParkInnovate.Core/Models/ParkingSpotOccupationModel/ParkingOccupationsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using SmartParkInnovate.Infrastructure.Data.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace SmartParkInnovate.Core.Models.ParkingSpotOccupationModel
+{
+    public class ParkingOccupationsViewModel
+    {
+        [Required]
+        public int ParkingSpotId { get; set; }
+
+        [Required]
+        [LicensePlateFormat]
+        public string VehicleLicensePlate { get; set; } = null!;
+
+        [Required]
+        public string VehicleOwnerEmail { get; set; } = null!;
+
+        [Required]
+        public string VehicleOwnerFullName { get; set; } = null!;
+
+        [Required]
+        public DateTime EnterDateTime { get; set; }
+
+        public DateTime? ExitDateTime { get; set; }
+    }
+}
